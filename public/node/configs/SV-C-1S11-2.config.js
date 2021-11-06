dea480ed6c303ad1e8068e507048c02cc8ab829b
Do95 <haku@Dev.local>
2021-11-05 23:03:40 -0600

    config sv-c-1s11-2

--- public/node/configs/SV-C-1S11-2.config.js
+++ public/node/configs/SV-C-1S11-2.config.js
@@ -3,43 +3,43 @@ const config = {
     bg: "1.jpg",
     items: {
       "field-1": {
-        x: 50,
-        y: 244,
-        width: 165,
+        x: 157,
+        y: 372,
+        width: 125,
         lineGap: 10,
-        max: 65
+        max: 26
       },
       "field-2": {
-        x: 50,
-        y: 244,
-        width: 165,
+        x: 160,
+        y: 489,
+        width: 130,
         lineGap: 10,
-        max: 65
+        max: 26
       },
       "field-3": {
-        x: 50,
-        y: 244,
-        width: 165,
+        x: 80,
+        y: 641,
+        width: 35,
         lineGap: 10,
-        max: 65
+        max: 2
       },
       "field-4": {
-        x: 50,
-        y: 244,
-        width: 165,
+        x: 370,
+        y: 267,
+        width: 65,
         lineGap: 10,
-        max: 65
+        max: 6
       },
       "field-5": {
-        x: 50,
-        y: 244,
-        width: 165,
+        x: 330,
+        y: 464,
+        width: 240,
         lineGap: 10,
-        max: 65
+        max: 50
       },
       "fecha": {
         x: 440,
-        y: 155,
+        y: 170,
         width: 220,
         lineGap: 11,
       }
@@ -49,27 +49,27 @@ const config = {
     bg: "2.jpg",
     items: {
       "field-6": {
-        x: 50,
-        y: 61,
-        width: 490,
+        x: 100,
+        y: 62,
+        width: 440,
         lineGap: 8,
         max: 640
       },
       "img": {
         image: true,
-        x: 120,
-        y: 290,
+        x: 130,
+        y: 260,
         config: {
           fit: [400, 200],
           align: 'center'
         },
       },
       "field-7": {
-        x: 50,
-        y: 61,
+        x: 185,
+        y: 775,
         width: 490,
         lineGap: 8,
-        max: 640
+        max: 70
       },
     }
   },
