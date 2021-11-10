e05b62fd213ba0e8aabe2bdc19bc4e5a2b347c14
Do95 <haku@Dev.local>
2021-11-09 19:10:38 -0600

    new sv-c-1s11-6.config.js

--- public/node/configs/SV-C-1S11-6.config.js
+++ public/node/configs/SV-C-1S11-6.config.js
@@ -3,77 +3,77 @@ const config = {
     bg: "1.jpg",
     items: {
       "field-1": {
-        x: 35,
-        y: 237,
+        x: 237,
+        y: 214,
         width: 188,
         lineGap: 4,
-        max: 40
+        max: 6
       },
       "field-1-copy": {
-        x: 35,
-        y: 237,
+        x: 220,
+        y: 236,
         width: 188,
         lineGap: 4,
-        max: 40
+        max: 6
       },
       "field-2": {
-        x: 45,
-        y: 360,
+        x: 233,
+        y: 281,
         width: 258,
         lineGap: 10,
-        max: 100
+        max: 7
       },
       "field-2-copy": {
-        x: 35,
-        y: 237,
+        x: 234,
+        y: 303,
         width: 188,
         lineGap: 4,
-        max: 40
+        max: 7
       },
       "field-3": {
-        x: 45,
-        y: 462,
+        x: 237,
+        y: 347,
         width: 258,
         lineGap: 10,
-        max: 100
+        max: 3
       },
       "field-3-copy": {
-        x: 35,
-        y: 237,
+        x: 219,
+        y: 370,
         width: 188,
         lineGap: 4,
-        max: 40
+        max: 3
       },
       "field-4-1": {
         checkbox: true,
-        x: 58,
-        y: 695,
+        x: 200,
+        y: 473,
       },
       "field-4-2": {
         checkbox: true,
-        x: 58,
-        y: 717,
+        x: 200,
+        y: 494,
       },
       "field-4-3": {
         checkbox: true,
-        x: 58,
-        y: 740,
+        x: 200,
+        y: 516,
       },
       "field-4-4": {
         checkbox: true,
-        x: 58,
-        y: 760,
+        x: 200,
+        y: 538,
       },
       "field-5": {
-        x: 330,
-        y: 272,
-        width: 216,
-        lineGap: 4,
-        max: 46
+        x: 407,
+        y: 249,
+        width: 159,
+        lineGap: 12,
+        max: 325
       }, 
       "fecha": {
-        x: 455,
-        y: 165,
+        x: 470,
+        y: 171,
         width: 220,
         lineGap: 11,
       }
@@ -87,20 +87,20 @@ const config = {
         y: 60,
         width: 490,
         lineGap: 8,
-        max: 690
+        max: 695
       },
       "img": {
         image: true,
         x: 100,
-        y: 260,
+        y: 255,
         config: {
           fit: [400, 200],
           align: 'center'
         },
       },
       "field-7": {
-        x: 175,
-        y: 775,
+        x: 186,
+        y: 773,
         width: 330,
         lineGap: 6,
         max: 70
